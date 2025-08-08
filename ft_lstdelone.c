bcfdac5b3309ca942dd39a07e7cc2c4163e79b90
okraus <okraus@student.42prague.com>
2025-08-08 15:51:33 +0200

    lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,10 +6,11 @@
 /*   By: okraus <okraus@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 12:27:59 by okraus            #+#    #+#             */
-/*   Updated: 2025/05/28 14:48:20 by okraus           ###   ########.fr       */
+/*   Updated: 2025/08/08 15:51:11 by okraus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
